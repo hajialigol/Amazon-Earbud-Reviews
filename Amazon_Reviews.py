ccb5ef9e35414583861bd8b764cf9f3340bd0a18
Daniel <danielhajialigol@gmail.com>
2020-06-20 18:02:30 -0400

    Creating source code for Jupyter Notebook

---
+++ Amazon_Reviews.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+amazon_reviews = pd.read_csv("AllProductReviews.csv")
+
+
+# Find the amount of words for a given review
+def amount_of_words(review):
+    word_list = review.split()
+    length_of_words = [len(word) for word in word_list]
+    amount_of_words = sum(length_of_words)
+    return amount_of_words
+
+# Generate the average word length for a given review
+def avg_word_length(review):
+    word_list = review.split()
+    length_of_words = [len(word) for word in word_list]
+    number_of_words = len(word_list)
+    if number_of_words == 0:
+        return None
+    else:
+        return sum(length_of_words)/number_of_words
